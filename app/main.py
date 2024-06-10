75288820132d54c96de1a5aaff8f399c7ca2cf50
Wilder <w.cossio.r@gmail.com>
2024-06-10 17:45:04 -0400

    configurando variables de entorno y el gitignore

--- app/main.py
+++ app/main.py
@@ -6,9 +6,12 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from flask import Flask
 from app.routes import register_routes
 from app.models import db
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://wil:123456@db/my_collections'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
